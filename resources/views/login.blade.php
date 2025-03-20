189591684f0a3261f91e69506afb9f86e826c9d1
Cepi-Max <cepihermawan818@gmail.com>
2025-03-20 16:07:25 +0700

    ccobalagi

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -11,21 +11,21 @@
 <body>
     
     <section class="vh-100" style="background-color: #508bfc;">
-        <h3 class="mb-5">Silahkan login</h3>
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $item)
-                        <li>{{ $item }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
         <div class="container py-5 h-100">
-          <div class="row d-flex justify-content-center align-items-center h-100">
-            <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-              <div class="card shadow-2-strong" style="border-radius: 1rem;">
-                <div class="card-body p-5">
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                    <div class="card shadow-2-strong" style="border-radius: 1rem;">
+                        <h3 class="mb-5">Silahkan login</h3>
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $item)
+                                        <li>{{ $item }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+                        <div class="card-body p-5">
 
                   <form action="{{ route('login') }}" method="post">
                     <div class="form-group">
