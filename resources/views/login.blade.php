c24a602a21d66865f1686b37d8ee4ce072e8cc71
Cepi-Max <cepihermawan818@gmail.com>
2025-03-20 16:08:21 +0700

    ccobalagi

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -15,7 +15,7 @@
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-12 col-md-8 col-lg-6 col-xl-5">
                     <div class="card shadow-2-strong" style="border-radius: 1rem;">
-                        <h3 class="mb-5">Silahkan login</h3>
+                        <h5 class="mb-5 text-center">Silahkan login</h5>
                         @if ($errors->any())
                             <div class="alert alert-danger">
                                 <ul>
